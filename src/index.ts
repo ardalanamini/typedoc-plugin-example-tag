2ecc82c6df1de669f4ad5e324b87dc3fca7d600c
Ardalan Amini <ardalanamini22@gmail.com>
2022-05-07 00:46:04 +0430

    deps: Update dependencies
    Fixes #1

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,49 @@
-import * as plugin from "./plugin";
+import { Application, Context, Converter, ParameterType, ReflectionKind } from "typedoc";
 
-export = (pluginHost: any) => {
-  const app = pluginHost.owner;
+export function load(app: Application) {
+  app.options.addDeclaration({
+    type: ParameterType.String,
+    name: "preferred-example-language",
+    help: "[Example Tag Plugin] Specifies the preferred example tag language.",
+    defaultValue: "typescript",
+  });
 
-  app.options.addDeclaration({ name: "preferred-example-language" });
+  app.converter.on(Converter.EVENT_RESOLVE_BEGIN, (context: Context) => {
+    const preferredLanguage = app.options.getValue("preferred-example-language");
 
-  app.converter.addComponent("example-tag", plugin.ExampleTagPlugin);
-};
+    const reflections = context.project.getReflectionsByKind(ReflectionKind.All);
+
+    for (const reflection of reflections) {
+      const comment = reflection.comment;
+
+      if (comment?.tags == null) continue;
+
+      const indexes = comment.tags.reduce<number[]>((prev, tag, index) => {
+        if (tag.tagName === "example") prev.push(index);
+
+        return prev;
+      }, []);
+
+      const length = indexes.length;
+
+      if (length === 1) {
+        const index = indexes[0];
+
+        comment.text += `#### Example\n\`\`\`${preferredLanguage}${comment.tags[index].text}\`\`\``;
+        comment.tags.splice(index);
+
+        continue;
+      }
+
+      let counter = 0;
+
+      for (const index of indexes) {
+        comment.text += `#### Example ${++counter}\n\`\`\`${preferredLanguage}${comment.tags ? comment.tags[index].text : ""}\`\`\``;
+
+        if (counter !== length) comment.text += "\n";
+      }
+
+      indexes.reverse().forEach((index) => comment.tags.splice(index));
+    }
+  });
+}
